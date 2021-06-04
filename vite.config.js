61b677e4df79d20ddd630aa6374c59001ccba721
abhijit-hota <abhihota025@gmail.com>
2021-06-04 18:18:23 +0530

    add: linting and legacy support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+	plugins: [
+		reactRefresh(),
+		legacy({
+			targets: ["ie >= 11"],
+			additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+		}),
+	],
+});
